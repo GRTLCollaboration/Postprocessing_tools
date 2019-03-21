9325d3c4c7bd6722f85bdeb6bc0323131006a0f7
KAClough <katyclough@yahoo.com>
2019-03-21 12:14:41 +0000

    Add reprocessing tool for chombo files

---
+++ ChomboTools/ReprocessFiles/ReprocessingLevel.hpp
@@ -0,0 +1,36 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef REPROCESSINGLEVEL_HPP_
+#define REPROCESSINGLEVEL_HPP_
+
+#include "GRAMRLevel.hpp"
+
+class ReprocessingLevel : public GRAMRLevel
+{
+    friend class DefaultLevelFactory<ReprocessingLevel>;
+    // Inherit the contructors from GRAMRLevel
+    using GRAMRLevel::GRAMRLevel;
+
+    // initialize data
+    virtual void initialData() { m_state_new.setVal(0.); }
+
+    void postRestart()
+    {
+        // Add code here to do what you need it to do on each level
+        pout() << "The time is " << m_time << " on level " << m_level 
+               << ". Your wish is my command." << endl;
+    }
+
+    virtual void specificEvalRHS(GRLevelData &a_soln, GRLevelData &a_rhs,
+                                 const double a_time)
+    {
+    }
+
+    virtual void computeTaggingCriterion(FArrayBox &tagging_criterion,
+                                         const FArrayBox &current_state){};
+};
+
+#endif /* REPROCESSINGLEVEL_HPP_ */
---
+++ ChomboTools/ReprocessFiles/SimulationParameters.hpp
@@ -0,0 +1,42 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef SIMULATIONPARAMETERS_HPP_
+#define SIMULATIONPARAMETERS_HPP_
+
+// General includes
+#include "GRParmParse.hpp"
+#include "ChomboParameters.hpp"
+
+class SimulationParameters : public ChomboParameters
+{
+  public:
+    // For the Interpolator test we don't need many parameters
+    SimulationParameters(GRParmParse &pp) : ChomboParameters(pp)
+    {
+        // read the problem specific params
+        readParams(pp);
+    }
+
+    void readParams(GRParmParse &pp)
+    {
+        // Grid setup
+        pp.get("num_files", num_files);
+        pp.get("start_file", start_file);
+        pp.get("checkpoint_interval", checkpoint_interval);
+
+        // extraction params
+        dx.fill(coarsest_dx);
+        origin.fill(coarsest_dx / 2.0);
+
+        plot_interval = 1;
+    }
+
+    int num_files, start_file, checkpoint_interval;
+    std::array<double, CH_SPACEDIM> origin,
+        dx; // location of coarsest origin and dx
+};
+
+#endif /* SIMULATIONPARAMETERS_HPP_ */
