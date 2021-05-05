5a9754f0f2bfbbcb5b61581fb72fa23f2cf032ae
KAClough <katyclough@yahoo.com>
2021-05-05 11:13:59 +0100

    Update example for diagnostic use and to give explicit extraction example

--- ChomboTools/ReprocessFiles/SimulationParameters.hpp
+++ ChomboTools/ReprocessFiles/SimulationParameters.hpp
@@ -7,34 +7,25 @@
 #define SIMULATIONPARAMETERS_HPP_
 
 // General includes
-#include "GRParmParse.hpp"
 #include "ChomboParameters.hpp"
+#include "GRParmParse.hpp"
 
-class SimulationParameters : public ChomboParameters
-{
-  public:
-    // For the Interpolator test we don't need many parameters
-    SimulationParameters(GRParmParse &pp) : ChomboParameters(pp)
-    {
-        // read the problem specific params
-        readParams(pp);
-    }
+class SimulationParameters : public ChomboParameters {
+public:
+  // For the Interpolator test we don't need many parameters
+  SimulationParameters(GRParmParse &pp) : ChomboParameters(pp) {
+    // read the problem specific params
+    readParams(pp);
+  }
 
-    void readParams(GRParmParse &pp)
-    {
-        // Grid setup
-        pp.get("num_files", num_files);
-        pp.get("start_file", start_file);
-        pp.get("checkpoint_interval", checkpoint_interval);
+  void readParams(GRParmParse &pp) {
+    // Grid setup
+    pp.get("num_files", num_files);
+    pp.get("start_file", start_file);
+    pp.get("checkpoint_interval", checkpoint_interval);
+  }
 
-        // extraction params
-        dx.fill(coarsest_dx);
-        origin.fill(coarsest_dx / 2.0);
-    }
-
-    int num_files, start_file, checkpoint_interval;
-    std::array<double, CH_SPACEDIM> origin,
-        dx; // location of coarsest origin and dx
+  int num_files, start_file, checkpoint_interval;
 };
 
 #endif /* SIMULATIONPARAMETERS_HPP_ */
