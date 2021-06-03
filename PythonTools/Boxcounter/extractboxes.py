fa2af5b84302688527d8f26a65c035c8bedfc56c
amelialdrew <ad652@cam.ac.uk>
2021-06-03 15:13:28 +0100

    Add MPI box count tools (#15)
    * Add MPI box count tools
    
    * Minor typo
    
    * Fixed typo in processing.sh so that any number of levels is read correctly. Put into directory Boxcounter with README
    
    Co-authored-by: Amelia Drew <dc-drew1@cosmos2.private.damtp.cam.ac.uk>

---
+++ PythonTools/Boxcounter/extractboxes.py
@@ -0,0 +1,38 @@
+import csv
+import matplotlib.pyplot as plt
+
+'''Run processing.sh with pout.0 - outputs level, time, boxes on proc 0, total boxes'''
+'''Only works on output with verbosity 0'''
+
+i=0
+
+data = open('pout.0_parsed')
+with data as csvfile:
+    filereader = csv.reader(csvfile, delimiter=',')
+    timelist = [] #col 1
+    totalboxlist = [] #col 3
+
+    '''Hardcoded max level to plot'''
+    while i < 10:
+        for row in filereader:
+            if int(row[0]) == i:
+                '''Can input max time'''
+ #              if float(row[1]) < 300:
+                timelist.append(float(row[1]))
+                totalboxlist.append(int(row[3]))
+
+        plt.plot(timelist, totalboxlist, label='%s' % i)
+        plt.legend()
+        plt.savefig('boxplot_level_%s.png' % i)
+
+        timelist.clear()
+        totalboxlist.clear()
+
+        '''Resets file loop'''
+        data.seek(0)
+        
+        i+=1
+
+    print('%s' % i)
+
+
