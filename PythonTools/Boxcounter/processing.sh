1b88d469049970c030f0c57a30ea9aebbec5915c
Amelia Drew <dc-drew1@cosmos2.private.damtp.cam.ac.uk>
2021-06-10 14:36:43 +0100

    Fixed typo to add comma in pout.0_parsed and removed old scripts from PythonTools/ so just in PythonTools/Boxcounter

--- PythonTools/Boxcounter/processing.sh
+++ PythonTools/Boxcounter/processing.sh
@@ -2,7 +2,7 @@
 
 sed -n '/GRAMRLevel::/,$p' ./pout.0 > ./pout.0_temp
 sed -e 's!GRAMRLevel::advance level !!' ./pout.0_temp > ./pout.0_temp2
-sed -r 's/\s+/, /' ./pout.0_temp2 > ./pout.0_temp
+sed -r 's/ +/, /' ./pout.0_temp2 > ./pout.0_temp
 sed -e 's!at time !!' ./pout.0_temp > ./pout.0_temp2
 sed -e 's/ (.*:/,/' ./pout.0_temp2 > ./pout.0_temp
 sed -e 's/ \//,/' ./pout.0_temp > ./pout.0_parsed
